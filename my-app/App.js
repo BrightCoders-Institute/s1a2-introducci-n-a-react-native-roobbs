66cd11b8dc8c3446a2a3c536a88a0bfb9db10648
Carlos <carlossalmeroncabrera@gmail.com>
2023-12-17 00:31:22 -0600

    Agregue un saludo

--- my-app/App.js
+++ my-app/App.js
@@ -2,9 +2,49 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  function sayHi() {
+    const nombre = prompt('Ingresa tu nombre:');
+    if (nombre) {
+      alert(`Hola, ${nombre}!`);
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hola Mundo</Text>
+      <h2>Como usar React</h2>
+      <button onClick={sayHi}>Greet</button>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'blue',
+  },
+});
+
+/*
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function App() {
+  const mostrarMensaje = () => {
+    const nombre = prompt('Ingresa tu nombre:');
+    if (nombre) {
+      alert(`Hola, ${nombre}!`);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>Hola Mundo</Text>
+      <Button title="Mostrar Mensaje" onPress={mostrarMensaje} />
       <StatusBar style="auto" />
     </View>
   );
@@ -18,3 +58,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+*/
